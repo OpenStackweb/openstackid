be57049b0ebe47cbfbc8fcc14690707c95d0f668
smarcet <smarcet@gmail.com>
2020-09-15 16:06:44 -0300

    Fixed Email wording
    Change-Id: Iadf30969b35e340a3e30337e35fba69b5941197b
    Signed-off-by: smarcet <smarcet@gmail.com>

--- resources/views/emails/auth/email_verification_request_success.blade.php
+++ resources/views/emails/auth/email_verification_request_success.blade.php
@@ -6,6 +6,7 @@
 <body>
 <p>Dear {!!$user_fullname!!},</p>
 <p>Your email address has been successfully verified.</p>
+<p>If you need further assistance, please email <a href="mailto:{!! Config::get('app.help_email') !!}">{!! Config::get('app.help_email') !!}</a></p>
 <br/>
 <br/>
 <p>Cheers,<br/>Your {!! Config::get('app.tenant_name') !!} Support Team</p>
