6991365448edb048bc668aa8e3d08932807dd0fd
smarcet <smarcet@gmail.com>
2020-09-16 11:11:27 -0300

    updated welcome email
    Change-Id: I90d71ab116c17d15dbfee01ef2d864fdf65bbe7a
    Signed-off-by: smarcet <smarcet@gmail.com>

--- resources/views/emails/welcome_new_user_email.blade.php
+++ resources/views/emails/welcome_new_user_email.blade.php
@@ -5,12 +5,11 @@
 </head>
 <body>
 <p>Dear {!!$user_fullname!!},</p>
-<p>Thank you for registering for a new {!! Config::get('app.app_name') !!} account. We are glad you’re here!</p>
-<p>You will use this account to access all {!! Config::get('app.tenant_name') !!} apps that require an FNid to access. Your user details are associated with your {!! Config::get('app.app_name') !!} and you are able to grant access to that information to each app at your discretion.</p>
-<p>To edit your profile just click <a href="{!! URL::action("UserController@getLogin") !!}">here</a>. You may update your photo, add a bio, and other information you wish to share.</p>
-<p>Should you have any questions, please email <a href="mailto:{!! Config::get('app.help_email') !!}">{!! Config::get('app.help_email') !!}</a>for assistance.
+<p>Thank you for registering for a new {!! Config::get('app.app_name') !!}. You will use this account to access virtual events and other apps.</p>
+<p>When using your {!! Config::get('app.app_name') !!} to log in to {!! Config::get('app.tenant_name') !!} virtual events and applications, you will be asked to "accept" permissions so the application can use your information to function accordingly.</p>
+<p>Should you have any questions, please email <a href="mailto:{!! Config::get('app.help_email') !!}">{!! Config::get('app.help_email') !!}</a>for assistance.</p>
 <br/>
 <br/>
-<p>Thanks!,<br/>Your {!! Config::get('app.tenant_name') !!} Support Team</p>
+<p>Thanks!<br/>Your {!! Config::get('app.tenant_name') !!} Support Team</p>
 </body>
 </html>
