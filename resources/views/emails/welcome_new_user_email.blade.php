8c18c699200e068367ef85dadd5a6d0d1248fedc
smarcet <smarcet@gmail.com>
2020-09-16 11:24:30 -0300

    Fixed typo
    Change-Id: Ie3c8e30b9fe08ad320b597231f6579ffe596bb22
    Signed-off-by: smarcet <smarcet@gmail.com>

--- resources/views/emails/welcome_new_user_email.blade.php
+++ resources/views/emails/welcome_new_user_email.blade.php
@@ -7,7 +7,7 @@
 <p>Dear {!!$user_fullname!!},</p>
 <p>Thank you for registering for a new {!! Config::get('app.app_name') !!}. You will use this account to access virtual events and other apps.</p>
 <p>When using your {!! Config::get('app.app_name') !!} to log in to {!! Config::get('app.tenant_name') !!} virtual events and applications, you will be asked to "accept" permissions so the application can use your information to function accordingly.</p>
-<p>Should you have any questions, please email <a href="mailto:{!! Config::get('app.help_email') !!}">{!! Config::get('app.help_email') !!}</a>for assistance.</p>
+<p>Should you have any questions, please email <a href="mailto:{!! Config::get('app.help_email') !!}">{!! Config::get('app.help_email') !!}</a> for assistance.</p>
 <br/>
 <br/>
 <p>Thanks!<br/>Your {!! Config::get('app.tenant_name') !!} Support Team</p>
