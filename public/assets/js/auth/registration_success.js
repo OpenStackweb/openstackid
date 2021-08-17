38a940b83574585867779a1c7cb41790d994219b
smarcet@gmail.com <smarcet@gmail.com>
2021-08-17 18:10:56 -0300

    Post Registration message adjustment
    fixed wording
    
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I5a686740eeb6c37d98a5da730df51c2c70f82c97

--- public/assets/js/auth/registration_success.js
+++ public/assets/js/auth/registration_success.js
@@ -5,7 +5,7 @@
         var redirect = $('#redirect_url');
         if(redirect.length > 0){
             var href = $(redirect).attr('href');
-            setTimeout(function(){ window.location = href; }, 3000);
+            setTimeout(function(){ window.location = href; }, 10000);
         }
 
     });
--- resources/views/auth/register_success.blade.php
+++ resources/views/auth/register_success.blade.php
@@ -11,12 +11,10 @@
 @section('content')
     <div class="container">
         <div class="well">
-            <p> Thank you for creation an {!! Config::get("app.app_name") !!}. You will be receiving an email to verify your email address and
-            then another one confirming you have successfully created your {!! Config::get("app.app_name") !!} after your email is verified.</p>
+            <p> Thank you for creating an {!! Config::get("app.app_name") !!}. You will be receiving an email to verify your email address and
+            then another confirming you have successfully created your {!! Config::get("app.app_name") !!} after your email is verified.</p>
             @if($redirect_uri)
                 <p>Now you will be redirected to <a id="redirect_url" name="redirect_url" href="{{$redirect_uri}}">{{$redirect_uri}}</a></p>
-            @else
-                <p> Meanwhile you could check <a href="{!! URL::action("UserController@getLogin") !!}">Login</a>.</p>
             @endif
         </div>
     </div>
