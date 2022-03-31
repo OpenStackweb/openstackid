6885813e5585433a67c37a8c8fdc389546ce10de
Román Gutierrez <roman.gutierrez@gmail.com>
2022-03-31 10:59:16 -0300

    change template format and text (#4)
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- resources/views/emails/auth/reset_password_successfull.blade.php
+++ resources/views/emails/auth/reset_password_successfull.blade.php
@@ -4,23 +4,13 @@
     <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
         <tbody>
         <tr>
-            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
-                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">Dear <a href="#" style="text-decoration:none !important;color:black !important; cursor:default !important">{!!$user_email!!}</a>,</div>
-            </td>
-        </tr>
-        <tr>
             <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">You are receiving this email because you changed your password.</div>
+                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">The password was reset for {{Config::get('app.name')}} <a href="#" style="text-decoration:none !important;color:black !important; cursor:default !important; font-weight:bold">{!!$user_email!!}</a>.</div>
             </td>
         </tr>
         <tr>
             <td align="center" style="font-size:0px;padding:10px 25px;padding-right:16px;padding-left:25px;word-break:break-word;">
-                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">If you did not request a password reset, please get in touch with us.</div>
-            </td>
-        </tr>
-        <tr>
-            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
-                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">Thanks! <br/><br/>{{Config::get('app.tenant_name')}} Support Team</div>
+                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">If you did not reset your password, please email <a href="mailto:{{Config::get('app.help_email')}}" style="text-decoration:none !important">{{Config::get('app.help_email')}}</a>.</div>
             </td>
         </tr>
         </tbody>
