2b0fcf890ddcd9aeb14a20a44edaa8fceb4afaed
smarcet@gmail.com <smarcet@gmail.com>
2022-06-14 13:10:06 -0500

    Fix on email template
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I3627eca502b5d474ace8c316ad8ce58453dd2a2d

--- resources/views/emails/welcome_new_user_email_otp_fn.blade.php
+++ resources/views/emails/welcome_new_user_email_otp_fn.blade.php
@@ -4,12 +4,20 @@
     <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
         <tbody>
         <tr>
-            <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
-                <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">
-                    Thank you for using a one-time-use code to verify your email address@if(!empty($site_base_url)) on
-                    {!! $site_base_url !!}@endif . You now have an {!! Config::get('app.app_name') !!} using the email address <a href="#" style="text-decoration:none !important;color:black !important; cursor:default !important">{!!$user_email!!}</a>.
-                </div>
-            </td>
+            @if(!empty($site_base_url))
+                <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                    <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">
+                        Thank you for using a one-time-use code to verify your email address on
+                        {!! $site_base_url !!}. You now have an {!! Config::get('app.app_name') !!} using the email address <a href="#" style="text-decoration:none !important;color:black !important; cursor:default !important">{!!$user_email!!}</a>.
+                    </div>
+                </td>
+            @else
+                <td align="center" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                    <div style="font-family:open Sans Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#000000;">
+                        Thank you for using a one-time-use code to verify your email address. You now have an {!! Config::get('app.app_name') !!} using the email address <a href="#" style="text-decoration:none !important;color:black !important; cursor:default !important">{!!$user_email!!}</a>.
+                    </div>
+                </td>
+            @endif
         </tr>
         @if(!empty($reset_password_link))
         <tr>
