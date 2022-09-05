0d6b8ccc60a8230153497d9ad24f4189640cc716
smarcet@gmail.com <smarcet@gmail.com>
2022-09-05 11:17:39 -0500

    Updated private user serializer
    added missing fields
    
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>
    Change-Id: I4d4a34021104ff8c2f04fd0e87dedf4f53cc47b9

--- app/ModelSerializers/Auth/UserSerializer.php
+++ app/ModelSerializers/Auth/UserSerializer.php
@@ -30,29 +30,55 @@ class BaseUserSerializer extends BaseSerializer
     ];
 }
 
+/**
+ * Class PublicUserSerializer
+ * @package App\ModelSerializers\Auth
+ */
 final class PublicUserSerializer extends BaseUserSerializer
 {
 
 }
 
+/**
+ * Class PrivateUserSerializer
+ * @package App\ModelSerializers\Auth
+ */
 final class PrivateUserSerializer extends BaseUserSerializer
 {
 
     protected static $array_mappings = [
         'Email' => 'email:json_string',
+        'Identifier' => 'identifier:json_string',
+        'EmailVerified' => 'email_verified:json_boolean',
         'Bio' => 'bio:json_string',
+        'Address1' => 'address1:json_string',
+        'Address2' => 'address2:json_string',
+        'City' => 'city:json_string',
+        'State' => 'state:json_string',
+        'PostCode' => 'post_code:json_string',
+        'CountryIsoCode' => 'country_iso_code:json_string',
+        'SecondEmail' => 'second_email:json_string',
+        'ThirdEmail' => 'third_email:json_string',
+        'Gender' => 'gender:json_string',
+        'GenderSpecify' => 'gender_specify:json_string',
+        'StatementOfInterest' => 'statement_of_interest:json_string',
+        'Irc' => 'irc:json_string',
+        'LinkedInProfile' => 'linked_in_profile:json_string',
+        'GithubUser' => 'github_user:json_string',
+        'WechatUser' => 'wechat_user:json_string',
+        'TwitterName' => 'twitter_name:json_string',
+        'Language' => 'language:json_string',
+        'DateOfBirth' => 'birthday:datetime_epoch',
+        'PhoneNumber' => 'phone_number:json_string',
         'Company' => 'company:json_string',
         'JobTitle' => 'job_title:json_string',
-        'Gender' => 'gender:json_string',
         'SpamType' => 'spam_type:json_string',
-        'Identifier' => 'identifier:json_string',
         'LastLoginDate' => 'last_login_date:datetime_epoch',
         'Active' => 'active:json_boolean',
-        'EmailVerified' => 'email_verified:json_boolean',
+        'PublicProfileShowPhoto' => 'public_profile_show_photo:json_boolean',
+        'PublicProfileShowFullname' => 'public_profile_show_fullname:json_boolean',
         'PublicProfileShowEmail' => 'public_profile_show_email:json_boolean',
         'PublicProfileAllowChatWithMe' => 'public_profile_allow_chat_with_me:json_boolean',
-        'PublicProfileShowFullname' => 'public_profile_show_fullname:json_boolean',
-        'PublicProfileShowPhoto' => 'public_profile_show_photo:json_boolean'
     ];
 
     /**
