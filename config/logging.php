b8beea790e70ba71cee35615ecfe749170dc375f
smarcet <smarcet@gmail.com>
2024-05-15 02:39:41 -0300

    chore: updated dockerfile

--- config/logging.php
+++ config/logging.php
@@ -40,13 +40,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('/logs/laravel.log'),
+            'path' => storage_path('/ogs/laravel.log'),
             'level' => env('LOG_LEVEL', 'error'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('/logs/laravel.log'),
+            'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'error'),
             'days' => 7,
         ],
