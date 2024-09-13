69d55bbf653bcba1ce032d7895784e7b04cf4c5b
smarcet@gmail.com <smarcet@gmail.com>
2024-09-13 12:11:50 -0300

    refactor: access token lifetime
    Change-Id: I40033b47d1491301f05aa6fdd3f3520f207c30cd

--- app/Models/OAuth2/AccessToken.php
+++ app/Models/OAuth2/AccessToken.php
@@ -14,11 +14,10 @@
 
 use App\libs\Utils\DeviceInfoHelper;
 use Auth\User;
-use DateTime;
-use DateInterval;
-use DateTimeZone;
 use App\Models\Utils\BaseEntity;
 use Doctrine\ORM\Mapping AS ORM;
+use Illuminate\Support\Facades\Log;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repositories\DoctrineAccessTokenRepository")
  * @ORM\Table(name="oauth2_access_token")
@@ -142,27 +141,21 @@ class AccessToken extends BaseEntity {
     public function getRemainingLifetime():int
     {
         //check is refresh token is stills alive... (ZERO is infinite lifetime)
-        if (intval($this->lifetime) == 0) return 0;
+        if ($this->lifetime == 0) return 0;
         $created_at = clone $this->created_at;
-        $created_at->setTimezone(new DateTimeZone('UTC'));
-        $created_at->add(new DateInterval('PT' . intval($this->lifetime) . 'S'));
-        $now = new DateTime(gmdate("Y-m-d H:i:s", time()), new DateTimeZone("UTC"));
-        //check validity...
-        if ($now > $created_at)
-            return -1;
-        $seconds = abs($created_at->getTimestamp() - $now->getTimestamp());;
-        return $seconds;
+        $now = new \DateTime('now', new \DateTimeZone('UTC'));
+        $time_elapsed = abs($now->getTimestamp() - $created_at->getTimestamp());
+        Log::debug(sprintf("AccessToken::getRemainingLifetime id %s time_elapsed %s", $this->id, $time_elapsed));
+        return $this->lifetime > $time_elapsed ? $this->lifetime - $time_elapsed : -1;
     }
 
     /**
      * @return bool
      */
     public function isVoid():bool {
-        //check lifetime...
-        $created_at = $this->created_at;
-        $created_at->add(new DateInterval('PT' . intval($this->lifetime) . 'S'));
-        $now        = new DateTime(gmdate("Y-m-d H:i:s", time()), new DateTimeZone("UTC"));
-        return ($now > $created_at);
+        $remainingLifeTime =  $this->getRemainingLifetime() ;
+        Log::debug(sprintf("AccessToken::isVoid id %s remainingLifeTime %s", $this->id, $remainingLifeTime));
+        return $remainingLifeTime == -1;
     }
 
     /**
