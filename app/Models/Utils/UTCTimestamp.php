52394b17462de4ed6d9f3e5a6f964f43aadaa13f
romanetar <roman_ag@hotmail.com>
2025-06-05 21:12:13 +0200

    feat: update dependencies and base code to support laravel 12
    Signed-off-by: romanetar <roman_ag@hotmail.com>

--- app/Models/Utils/UTCTimestamp.php
+++ app/Models/Utils/UTCTimestamp.php
@@ -12,7 +12,11 @@
  * limitations under the License.
  **/
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\QueryException;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
 /**
  * Class UTCTimestamp
  * @package App\Models\Utils
@@ -21,15 +25,18 @@ class UTCTimestamp extends FunctionNode
 {
     public $arithmeticExpression;
 
-    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
         return 'UTC_TIMESTAMP()';
     }
 
-    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    /**
+     * @throws QueryException
+     */
+    public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 }
