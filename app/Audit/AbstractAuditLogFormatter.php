c019242f16ba7fe310586928255e02d2491e3efa
Matias Perrone <github@matiasperrone.com>
2025-12-22 22:14:03 +0000

    feat: add Doctrine logging generic classes

---
+++ app/Audit/AbstractAuditLogFormatter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Audit;
+
+/**
+ * Copyright 2025 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+abstract class AbstractAuditLogFormatter implements IAuditLogFormatter
+{
+    protected AuditContext $ctx;
+
+    final public function setContext(AuditContext $ctx): void
+    {
+        $this->ctx = $ctx;
+    }
+
+    protected function getUserInfo(): string
+    {
+        if (!$this->ctx) {
+            return 'Unknown (unknown)';
+        }
+
+        $user_name = 'Unknown';
+        if ($this->ctx->userFirstName || $this->ctx->userLastName) {
+            $user_name = trim(sprintf("%s %s", $this->ctx->userFirstName ?? '', $this->ctx->userLastName ?? '')) ?: 'Unknown';
+        } elseif ($this->ctx->userEmail) {
+            $user_name = $this->ctx->userEmail;
+        }
+
+        $user_id = $this->ctx->userId ?? 'unknown';
+        return sprintf("%s (%s)", $user_name, $user_id);
+    }
+
+    abstract public function format($subject, array $change_set): ?string;
+}
