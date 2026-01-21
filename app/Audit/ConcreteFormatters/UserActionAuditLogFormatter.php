e512e92d9cf58f19dcbb2f51470dd0de70430688
Matias Perrone <matias.perrone@exomindset.co>
2026-01-21 10:57:31 -0300

    Feature | Add UserAction formatter (#97)
    * chore: Add route formatter
    
    * Fix formatter functionality
    
    * chore: update non existent info messages
    
    ---------
    
    Co-authored-by: Matias Perrone <github@matiasperrone.com>

--- app/Audit/ConcreteFormatters/UserActionAuditLogFormatter.php
+++ app/Audit/ConcreteFormatters/UserActionAuditLogFormatter.php
@@ -18,16 +18,24 @@ class UserActionAuditLogFormatter extends AbstractAuditLogFormatter
         try {
             $id = $subject->getId() ?? 'unknown';
             $title = $subject->getUserAction() ?? 'Unknown UserAction';
+            $owner = $subject->getOwner();
+            $ownerFullName = $owner ? $owner->getFullName() : 'Unknown';
+            $ownerID = $owner ? $owner->getID() : 'N/A';
+            $realm = $subject->hasRealm() ? $subject->getRealm() : 'N/A';
+            $ip = $subject->getFromIp() ?? 'Unknown';
 
             switch ($this->event_type) {
+
                 case IAuditStrategy::EVENT_ENTITY_CREATION:
-                    return sprintf("UserAction (%s) for '%s' created by user %s", $id, $title, $this->getUserInfo());
+                    return sprintf("UserAction (%s) for '%s' which owner is \"%s (%s)\", with realm \"%s\" and IP \"%s\" was created by user %s", $id, $title, $ownerFullName, $ownerID, $realm, $ip, $this->getUserInfo());
                 case IAuditStrategy::EVENT_ENTITY_UPDATE:
                     $details = $this->buildChangeDetails($change_set);
-                    return sprintf("UserAction (%s) for '%s' updated: %s by user %s", $id, $title, $details, $this->getUserInfo());
+                    return sprintf("UserAction (%s) for '%s' which owner is \"%s (%s)\", with realm \"%s\" and IP \"%s\" was updated: %s by user %s", $id, $title, $ownerFullName, $ownerID, $realm, $ip, $details, $this->getUserInfo());
                 case IAuditStrategy::EVENT_ENTITY_DELETION:
-                    return sprintf("UserAction (%s) for '%s' deleted by user %s", $id, $title, $this->getUserInfo());
+                    return sprintf("UserAction (%s) for '%s' which owner is \"%s (%s)\", with realm \"%s\" and IP \"%s\" was deleted by user %s", $id, $title, $ownerFullName, $ownerID, $realm, $ip, $this->getUserInfo());
             }
+            return null;
+
         } catch (\Exception $ex) {
             Log::warning("UserAction error: " . $ex->getMessage());
         }
