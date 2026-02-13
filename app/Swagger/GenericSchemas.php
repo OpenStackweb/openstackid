2c2445e6178356893dac5795d47661ca18f6b1ca
Matias Perrone <matias.perrone@exomindset.co>
2026-02-13 13:51:51 -0300

    Chore | Initial setup for OpenAPI 3.1.2 documentation (#103)
    * chore: Initial setup for OpenAPI 3.1 documentation
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: Move to OpenAPI v3.1.2 so UI supports it
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: Add Basic schema which will be referenced by all the other Models schemas
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: change name for Base schema
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: Add requested changes
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: remove unneeded schema
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * update .env.example with right values for OpenAPI docs
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    * chore: change API name in config
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>
    
    ---------
    
    Signed-off-by: matiasperrone-exo <matias.perrone@exomindset.co>

---
+++ app/Swagger/GenericSchemas.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Swagger\schemas;
+
+use OpenApi\Attributes as OA;
+
+
+#[OA\Schema(
+    schema: 'PaginateDataSchemaResponse',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'total', type: 'integer', example: 6),
+        new OA\Property(property: 'per_page', type: 'integer', example: 5),
+        new OA\Property(property: 'current_page', type: 'integer', example: 1),
+        new OA\Property(property: 'last_page', type: 'integer', example: 2),
+    ],
+    description: 'Base pagination metadata'
+)]
+class PaginateDataSchemaResponseSchema
+{
+}
+
+#[OA\Schema(
+    schema: 'Base',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'id', type: 'integer', description: 'Unique identifier', example: 1),
+        new OA\Property(property: 'created_at', type: 'integer', description: 'Creation timestamp (epoch)', example: 1609459200),
+        new OA\Property(property: 'updated_at', type: 'integer', description: 'Last update timestamp (epoch)', example: 1609459200),
+    ],
+    description: 'Base serializer fields'
+)]
+class BaseSchema
+{
+}
