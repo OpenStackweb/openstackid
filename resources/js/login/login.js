3f48186bb7833fdd2cc15a1ec2ece4f977f3e2a8
matiasperrone-exo <matias.perrone@exomindset.co>
2026-05-08 22:04:52 +0000

    chore: Add PR's requested changes and Unit tests
    - Validation failure when cf-turnstile-response is missing
    - Login flow behaviour before/after CAPTCHA threshold
    - Rendering of Turnstile on the thresholded login screen
    - Auth form submission when Turnstile is expired or not solved

--- resources/js/login/login.js
+++ resources/js/login/login.js
@@ -183,7 +183,6 @@ const PasswordInputForm = ({
             <input type="hidden" value={userNameValue} id="username" name="username"/>
             <input type="hidden" value={csrfToken} id="_token" name="_token"/>
             <input type="hidden" value="password" id="flow" name="flow"/>
-            <input type="hidden" value={loginAttempts ?? 0} name="login_attempts"/>
             {shouldShowCaptcha() && captchaPublicKey &&
                 <Turnstile
                     className={styles.turnstile}
@@ -265,7 +264,6 @@ const OTPInputForm = ({
                 <input type="hidden" value="otp" id="flow" name="flow"/>
                 <input type="hidden" value={otpCode} id="password" name="password"/>
                 <input type="hidden" value="email" id="connection" name="connection"/>
-                <input type="hidden" value={loginAttempts ?? 0} name="login_attempts"/>
                 {shouldShowCaptcha() && captchaPublicKey &&
                     <Turnstile
                         className={styles.turnstile}
