d1a91dff54982883d334f745a0bd7d98643576ed
smarcet <smarcet@gmail.com>
2026-09-21 23:53:42 -0300

    chore(auth): drop unused imports from MFACookieManager

--- app/Http/Controllers/Traits/MFACookieManager.php
+++ app/Http/Controllers/Traits/MFACookieManager.php
@@ -13,11 +13,9 @@
  **/
 
 use Auth\User;
-use Exception;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Request;
-use Keepsuit\LaravelOpenTelemetry\Facades\Logger;
 use Utils\IPHelper;
 
 /**
